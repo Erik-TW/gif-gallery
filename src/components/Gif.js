c4d9eadf058dede70086b6547bef443221883841
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2021-05-15 15:12:41 +0200

    fixed delete button

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -3,13 +3,12 @@ import classes from './Gif.module.css';
 const Gif = (props) => {
     return (
         <div className={classes.polaroid}>
-            
+                        <div className={classes.deleteBtn}>
+            <button style={{justifyContent: 'end'}} className='btn  btn-danger mb-1'>X</button>
+            </div>
             <img className={classes.image} src={props.url} alt="gif" />
             <div className= {classes.container}>
                 <p>{props.title.length == 0 ? <i>Untitled</i> : props.title}</p>
-            </div>
-            <div className={classes.deleteBtn}>
-            <button style={{justifyContent: 'end'}} className='btn  btn-danger mb-1'>X</button>
             </div>
         </div>
     );
